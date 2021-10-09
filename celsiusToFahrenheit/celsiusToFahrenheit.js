6dc21c615f9a9fa149b91ceb73a813bca8e6fde1
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-10-09 18:51:55 -0300

    updated

---
+++ celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -0,0 +1,32 @@
+/* TRANSFORM DEGREE */
+
+function transformDegree(degree) {
+  const celsiusExists = degree.toUpperCase().includes('C')
+  const fahrenheitExists = degree.toUpperCase().includes('F')
+
+  if (!celsiusExists && !fahrenheitExists) {
+    throw new Error('grau não identificado')
+  }
+
+  // FLUXO IDEAL F -> C
+  let updatedDegree = Number(degree.toUpperCase().replace('F', ''))
+  let formula = fahrenheit => ((fahrenheit - 32) * 5) / 9
+  let degreeSign = 'C'
+
+  // FLUXO ALTERNATIVO C -> F
+  if (celsiusExists) {
+    updatedDegree = Number(degree.toUpperCase().replace('C', ''))
+    formula = celsius => (celsius * 9) / 5 + 32
+    degreeSign = 'F'
+  }
+
+  return formula(updatedDegree) + degreeSign
+}
+
+try {
+  console.log(transformDegree('50F'))
+  console.log(transformDegree('10c'))
+  console.log(transformDegree('50z'))
+} catch (error) {
+  console.error(error.message)
+}
