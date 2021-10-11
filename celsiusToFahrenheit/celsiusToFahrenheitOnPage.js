a57e364696c78ba2c190edf88548d665f61ace59
Huann Almeida <67878933+huannvictor@users.noreply.github.com>
2021-10-11 16:28:05 -0300

    corrected formula

--- celsiusToFahrenheit/celsiusToFahrenheitOnPage.js
+++ celsiusToFahrenheit/celsiusToFahrenheitOnPage.js
@@ -5,33 +5,27 @@ let transformButton = document
   .querySelector('#transformButton')
   .addEventListener('click', transform)
 
-console.log(degreeValue)
-console.log(degreeTypeCelsius)
-console.log(degreeTypeFahrenheit)
-
 // verification
 
-function transform() {
+function transform(converter) {
+  converter.preventDefault()
   let outDegreeValue = Number(degreeValue.value)
   let TransformedDegree
   let newDegreeType
 
   let degreeType = document.querySelector('#degreeType')
   if (degreeType.options[degreeType.selectedIndex].value == "celsius"){
-    degreeType = " celsius → "
-    TransformedDegree = (outDegreeValue - 32) * (5/9)
-    newDegreeType = " fahrenheit"
+    degreeType = "celsius → "
+      TransformedDegree = (outDegreeValue * (9/5) + 32)
+    newDegreeType = "fahrenheit"
   }else if (degreeType.options[degreeType.selectedIndex].value == "fahrenheit"){
-    degreeType = "º fahrenheit → "
-    TransformedDegree = (outDegreeValue*9)/5+32
-    newDegreeType = "º celsius"
+    degreeType = "fahrenheit → "
+    TransformedDegree = (outDegreeValue - 32) * (5/9)
+    newDegreeType = "celsius"
   }else{
     degreeType = " ⨂"
     alert('please, select a valid degree')
   }
-  let writeDegreeValue = (document.querySelector('#writeDegreeValue').innerHTML = outDegreeValue + degreeType)
-  let writeTransformedDegree = document.querySelector('#writeTransformedDegree').innerHTML = TransformedDegree.toFixed(2) + newDegreeType
+  let writeDegreeValue = (document.querySelector('#writeDegreeValue').innerHTML = outDegreeValue.toFixed(1)  + 'º ' +  degreeType)
+  let writeTransformedDegree = document.querySelector('#writeTransformedDegree').innerHTML = TransformedDegree.toFixed(1) + 'º ' + newDegreeType
 }
-
-// outputs
-
